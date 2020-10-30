70458225dcd8b6c5fce9a63bc779e8889424ba10
shinya kaneshiro <armageddn1@gmail.com>
2020-10-30 04:41:32 +0000

    Heroku_update_after_change3

--- app/controllers/attendance_overtime_controller.rb
+++ app/controllers/attendance_overtime_controller.rb
@@ -13,10 +13,15 @@ class AttendanceOvertimeController < ApplicationController
       worked_on: overtime_params["worked_on"],
       status: overtime_params["status"]
       )
+
     if valid_check.nil?
-      if overtime_params["scheduled_end_at(4i)"].present? && overtime_params["scheduled_end_at(5i)"].present?
-        target_hour = overtime_params["scheduled_end_at(4i)"]
-        target_min = overtime_params["scheduled_end_at(5i)"]
+      user_designated_work_end_time = User.find(overtime_params["applicant"]).designated_work_end_time
+      user_hour = user_designated_work_end_time.hour
+      
+      target_hour = overtime_params["scheduled_end_at(4i)"]
+      target_min = overtime_params["scheduled_end_at(5i)"]
+      
+      if target_hour.present? && target_min.present?
         if overtime_params["next_day_flag"] == "1"
           worked_on_next = overtime_params["worked_on"].to_datetime + 1
           scheduled_end_at = "#{worked_on_next} #{target_hour}:#{target_min}:00"
@@ -30,7 +35,10 @@ class AttendanceOvertimeController < ApplicationController
       overtime = AttendanceOvertime.new(overtime_params)
       overtime.scheduled_end_at = scheduled_end_at
       
-      if overtime.save
+      if (user_hour.to_i > target_hour.to_i) && overtime_params["next_day_flag"] == "0"
+        flash[:danger] = "指定勤務時間より後の時間を選択してください。"
+        redirect_to user_url(date: overtime.worked_on.beginning_of_month)
+      elsif overtime.save
         flash[:success] = "残業申請を送信しました。"
         redirect_to user_url(date: overtime.worked_on.beginning_of_month)
       else
@@ -42,6 +50,43 @@ class AttendanceOvertimeController < ApplicationController
       redirect_to user_url(date: overtime_params["worked_on"].to_date.beginning_of_month)
     end
   end
+
+  # 該当ユーザの指定勤務終了時間より前の申請を弾く機能を実装する前
+  # def create
+  #   valid_check = AttendanceOvertime.find_by(
+  #     applicant: overtime_params["applicant"],
+  #     worked_on: overtime_params["worked_on"],
+  #     status: overtime_params["status"]
+  #     )
+  #   if valid_check.nil?
+  #     if overtime_params["scheduled_end_at(4i)"].present? && overtime_params["scheduled_end_at(5i)"].present?
+  #       target_hour = overtime_params["scheduled_end_at(4i)"]
+  #       target_min = overtime_params["scheduled_end_at(5i)"]
+  #       if overtime_params["next_day_flag"] == "1"
+  #         worked_on_next = overtime_params["worked_on"].to_datetime + 1
+  #         scheduled_end_at = "#{worked_on_next} #{target_hour}:#{target_min}:00"
+  #       else
+  #         scheduled_end_at = "#{overtime_params["worked_on"]} #{target_hour}:#{target_min}:00"
+  #       end
+  #     else
+  #       scheduled_end_at = ""
+  #     end
+      
+  #     overtime = AttendanceOvertime.new(overtime_params)
+  #     overtime.scheduled_end_at = scheduled_end_at
+      
+  #     if overtime.save
+  #       flash[:success] = "残業申請を送信しました。"
+  #       redirect_to user_url(date: overtime.worked_on.beginning_of_month)
+  #     else
+  #       flash[:danger] = "申請処理が失敗しました。改めて実施してください。"
+  #       redirect_to user_url(date: overtime.worked_on.beginning_of_month)
+  #     end
+  #   else
+  #     flash[:info] = "既に申請中です。承認/否認/取り消し後に改めて実施してください。"
+  #     redirect_to user_url(date: overtime_params["worked_on"].to_date.beginning_of_month)
+  #   end
+  # end
 
   # DB保存時に9時間ズレるのを解消する前  
   # def create
